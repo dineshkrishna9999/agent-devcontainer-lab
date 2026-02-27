b059cff4029f7c70d5552bf761027478302a56f6
Karakambaka, Dinesh <dinesh.karakambaka@hidglobal.com>
2026-02-27 05:18:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Agents in your Terminal
---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# To start clean when doing various demos
+
+rm -rf .claude
+rm -rf .gemini
+rm -rf .github
+
+rm -f CLAUDE.md
+rm -f GEMINI.md
+rm -f AGENTS.md
